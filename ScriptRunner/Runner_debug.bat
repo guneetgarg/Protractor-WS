a143dc85e6938c21b434022241760dab235ee405
guneetgarg <guneetgarg@gmail.com>
2017-03-05 09:56:54 +0530

    POM restructure

---
+++ ScriptRunner/Runner_debug.bat
@@ -0,0 +1,1 @@
+protractor debug conf.js
--- conf.js
+++ conf.js
@@ -1,18 +1,26 @@
 // An example configuration file.
 exports.config = {
 	directConnect : true,
+	// seleniumAddress: 'http://localhost:4444/wd/hub',
 
 	// Capabilities to be passed to the webdriver instance.
 	capabilities : {
-		'browserName' : 'chrome'
+		browserName : 'chrome',
+		chromeOptions: {
+		    args: ['show-fps-counter=true']
+		  }
 	},
 
+	/*
+	  multiCapabilities: [{ browserName: 'firefox' }, { browserName: 'chrome' }]
+	 */
+
 	// Framework to use. Jasmine is recommended.
-	framework : 'jasmine',
+	framework : 'jasmine2',
 
 	// Spec patterns are relative to the current working directory when
 	// protractor is called.
-	specs : [ 'example_spec.js' ],
+	specs : [ './TestScript/SuperCalculator_spec.js' ],
 
 	// Options to be passed to Jasmine.
 	jasmineNodeOpts : {
