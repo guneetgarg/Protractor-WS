f4ee3ec15db712939d2b8d18b5c4dc1465b42d06
guneetgarg <guneetgarg@gmail.com>
2017-03-06 08:29:35 +0530

    Suite add

--- conf.js
+++ conf.js
@@ -20,10 +20,18 @@ exports.config = {
 
 	// Spec patterns are relative to the current working directory when
 	// protractor is called.
-	specs : [ './TestScript/*.js' ],
+	//	specs : [ './TestScript/*.js' ],
+	
+	
+	// Spec patterns are relative to the location of the spec file. They may include glob patterns.
+	  suites: {
+	    homepage: './TestScript/*.js',
+	    search: ['tests/e2e/contact_search/**/*Spec.js','tests/e2e/venue_search/**/*Spec.js']
+	  },
 
 	// Options to be passed to Jasmine.
 	jasmineNodeOpts : {
-		defaultTimeoutInterval : 30000
+		defaultTimeoutInterval : 30000,
+		 showColors: true // Use colors in the command line report.
 	}
 };
